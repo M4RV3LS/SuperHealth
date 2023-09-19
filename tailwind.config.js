c77d5a15cfbbc1f5602deeb11fca5e6dce1c7d4c
TimothySubekti0322 <18221063@std.stei.itb.ac.id>
2023-09-19 13:42:58 +0700

    Build tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   //content: ['./index.html'],
-  content: ["./src/**/*.{html,js}"],
+  content: ["./*.{html,js}"],
   theme: {
     extend: {},
   },
