ea8cf946706b4a19a6ef31832d8e5cd77b9f9c0e
Marvel Subekti <18221058@std.stei.itb.ac.id>
2023-09-19 18:53:04 +0700

    update marvel

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = "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"; // Paste your API key here
+const API_KEY = "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"; // Paste your API key here
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   //content: ['./index.html'],
-  content: ["./*.{html,js}"],
+  content: ["./*.{html,js}", "./node_modules/flowbite/**/*.js","./node_modules/tw-elements/dist/js/**/*.js", "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin'), require("tw-elements/dist/plugin.cjs"),darkMode: "class"
+],
 }
 
